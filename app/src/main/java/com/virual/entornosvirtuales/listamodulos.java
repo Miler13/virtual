7085e4ff986ddc592d6fac6bbe6d924e87c2054b
Miler13 <milerrojassiles@gmail.com>
2019-10-28 23:07:35 -0400

    aas
    as

---
+++ app/src/main/java/com/virual/entornosvirtuales/listamodulos.java
@@ -0,0 +1,13 @@
+package com.virual.entornosvirtuales;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listamodulos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listamodulos);
+    }
+}
